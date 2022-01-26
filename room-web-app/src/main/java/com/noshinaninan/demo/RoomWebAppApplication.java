04f2b77fe15d0dba3ba964257202e3e563f5cec5
Noshina Ninan <noshzann@gmail.com>
2022-01-26 13:30:27 -0500

    initial commit

---
+++ room-web-app/src/main/java/com/noshinaninan/demo/RoomWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.noshinaninan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomWebAppApplication.class, args);
+	}
+
+}
---
+++ room-web-app/src/test/java/com/noshinaninan/demo/RoomWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noshinaninan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
