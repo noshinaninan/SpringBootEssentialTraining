051a41237c9203d53967401d4324cb099523c914
Noshina Ninan <noshzann@gmail.com>
2022-02-03 16:55:05 -0500

    added Spring message consumer, spring rest repositories

---
+++ room-web-app/src/main/java/com/noshinaninan/demo/async/RoomCleanerListener.java
@@ -0,0 +1,37 @@
+package com.noshinaninan.demo.async;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.noshinaninan.demo.models.Room;
+import com.noshinaninan.demo.services.RoomService;
+
+@Component
+public class RoomCleanerListener {
+    private static final Logger LOG = LoggerFactory.getLogger(RoomCleanerListener.class);
+
+    private final ObjectMapper mapper;
+    private final RoomService roomService;
+
+    public RoomCleanerListener(ObjectMapper mapper, RoomService roomService) {
+        this.mapper = mapper;
+        this.roomService = roomService;
+    }
+
+    public void receiveMessage(String message){
+        try{
+            AsyncPayload payload = mapper.readValue(message, AsyncPayload.class);
+            if("ROOM".equals(payload.getModel())){
+                Room room = roomService.getById(payload.getId());
+                LOG.info("ROOM {}:{} needs to be cleaned", room.getNumber(), room.getName());
+            }else{
+                LOG.warn("Unknown model type");
+            }
+        }catch(JsonProcessingException e){
+            e.printStackTrace();
+        }
+    }
+}
